4ba13730e3344cae718eb588b0c268a965d6aa0d
NilsPvR <nilspvr@pm.me>
2024-12-19 21:27:31 +0100

    docs: comment for simple manager

--- src/extension/contentscript/_common/interfaces/simpleManager.ts
+++ src/extension/contentscript/_common/interfaces/simpleManager.ts
@@ -1,3 +1,7 @@
+/**
+ * Interface for classes with a fire and forget logic which manage some state
+ * or data.
+ */
 export default interface SimpleManager {
     initialize(): void;
     performAction(): Promise<void>;
