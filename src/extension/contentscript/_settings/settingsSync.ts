25b6a3f29de2ba33f3cdf0d28f54e83f03ab1ec2
NilsPvR <nilspvr@pm.me>
2024-12-19 21:26:17 +0100

    refactor: make settings sync a simple manager

--- src/extension/contentscript/_settings/settingsSync.ts
+++ src/extension/contentscript/_settings/settingsSync.ts
@@ -1,21 +1,31 @@
 import browser from 'webextension-polyfill';
 import { DisplayFormat } from '../types/display';
 import View from '../view/view';
+import SimpleManager from '../_common/interfaces/simpleManager';
 
-export default class SettingsSync {
-    constructor(public view: View) {}
+export default class SettingsSync implements SimpleManager {
+    constructor(public view: View, private displayState: DisplayFormat) {}
 
-    public updateDisplayOnDisplayEnabledChange(displayState: DisplayFormat) {
+
+    public initialize() {
+        return;
+    }
+
+    public async performAction() {
+        this.updateDisplayOnDisplayEnabledChange();
+    }
+
+    public updateDisplayOnDisplayEnabledChange() {
         browser.storage.local.onChanged.addListener((changes: browser.Storage.StorageChange) => {
             if (isDisplayEnabledChange(changes)) {
-                this.addOrRemoveDisplay(displayState, changes.displayIsEnabled.newValue);
+                this.addOrRemoveDisplay(changes.displayIsEnabled.newValue);
             }
         });
     }
 
-    private async addOrRemoveDisplay(displayState: DisplayFormat, displayIsEnabled: boolean) {
+    private async addOrRemoveDisplay(displayIsEnabled: boolean) {
         if (displayIsEnabled) {
-            this.view.renderDisplay(displayState);
+            this.view.renderDisplay(this.displayState);
             return;
         }
         View.removeDisplay();
