deed616ed64bc5673af6391cec910d730e2892ff
NilsPvR <nilspvr@pm.me>
2025-03-11 16:28:19 +0100

    refactor: register handlers in function

--- src/extension/settings/settings.ts
+++ src/extension/settings/settings.ts
@@ -1,7 +1,25 @@
 import browser from 'webextension-polyfill';
 
-document.getElementById('privacy-policy-btn')!.addEventListener('click', openPrivacyPolicy);
+const PRIVACY_POLICY_BTN_ID = 'privacy-policy-btn';
 
-function openPrivacyPolicy() {
+(() => {
+    'use sctrict';
+    registerEvents();
+})();
+
+/**
+ * The script execution policy of the extension does not allow calling JS
+ * methods from the extension page. Just register all events here.
+ */
+function registerEvents() {
+    document
+        .getElementById(PRIVACY_POLICY_BTN_ID)!
+        .addEventListener('click', handlePrivacyPolicyClick);
+}
+
+/* ========================================================================
+    >>>>>>>>>>>>>>>>>>>> Event Handlers <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+function handlePrivacyPolicyClick() {
     browser.tabs.create({ url: 'privacy-policy.html' });
 }
