45fba07195392d7605fc474564734ae28161792a
NilsPvR <nilspvr@pm.me>
2025-08-29 10:01:18 +0200

    feat: show display status more clearly
    fixes #57

--- src/extension/popup/popupscript.ts
+++ src/extension/popup/popupscript.ts
@@ -1,23 +1,39 @@
 import Settings from '../common/utils/settings';
 
+const STATUS_TEXT_ID = 'status-text';
+
 async function onReady() {
     initializeEnabledCheck();
 }
 
 async function initializeEnabledCheck() {
-    const checkbox = document.querySelector('input[name=on-off-switch]');
+    const checkbox: HTMLInputElement | null = document.querySelector('input[name=on-off-switch]');
 
     if (checkbox && 'checked' in checkbox) {
         checkbox.addEventListener('change', async () => {
+            const statusTextElement = document.getElementById(STATUS_TEXT_ID);
             if (checkbox.checked) {
                 await Settings.setDisplayEnabled(true);
+
+                if (statusTextElement) {
+                    statusTextElement.innerText = 'aktiviert';
+                }
             } else {
                 await Settings.setDisplayEnabled(false);
+
+                if (statusTextElement) {
+                    statusTextElement.innerText = 'deaktiviert';
+                }
             }
         });
 
         if (!(await Settings.displayIsEnabled())) {
             checkbox.checked = false;
+
+            const statusTextElement = document.getElementById(STATUS_TEXT_ID);
+            if (statusTextElement) {
+                statusTextElement.innerText = 'deaktiviert';
+            }
         }
     }
 }
